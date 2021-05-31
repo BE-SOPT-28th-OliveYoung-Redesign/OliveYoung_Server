8b3c06e65c5af645f03b5b08200518d21579c32c
socar-alvin <alvin@socar.kr>
2021-05-31 20:20:54 +0900

    ver3

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,13 +1,37 @@
 import express, { Request, Response } from "express";
-import list from "../models/list";
+import Product from "../models/list";
 const router = express.Router();
+
+ /**
+ * @route GET /product
+ * @desc Get all products list
+ * @access Public
+ */
 
 router.get("/", async(res, req) => {
   try{
-    const product = await list.find()
+    const product = await Product.find()
     res.json(product);
   } catch(error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
-}) 
+})
+
+ /**
+ * @route GET /product/rank
+ * @desc Get all friends list
+ * @access Public
+ */
+
+router.get("/:rank", async(res,req) =>{
+    try{
+        const product = await Product.findOne({
+            user: req.params.rank,
+        });
+        if (!product) return res.status(400).json({ msg: "product not found" });
+    } catch(error){
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+})
---
+++ src/models/list.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    product : [
+        {
+          rank: {
+            type: Number,
+          },
+          brandLabel: {
+            type: String,
+          },
+          listLabel: {
+            type: String,
+          },
+          mainImageView: {
+            type: String,
+          },
+          reviewLabel : {
+            type: String,
+          },
+        },
+      ],
+});
+
+export default mongoose.model<IUser & mongoose.Document>("User", UserSchema);
