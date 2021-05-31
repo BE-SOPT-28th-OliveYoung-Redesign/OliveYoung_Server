8dd451ca86d13e902fc2567d0acc266dd8b467df
jihwan <sgh002400@naver.com>
2021-05-31 20:19:37 +0900

    index.ts added

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
-import list from "../models/list";
+import Product from "../models/list";
 const router = express.Router();
 
 router.get("/", async(res, req) => {
   try{
-    const product = await list.find()
+    const product = await Product.find()
     res.json(product);
   } catch(error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
-}) 
+});
+
