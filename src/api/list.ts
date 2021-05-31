47a48988238deb69234f7d94db8625aef19861bd
jihwan <sgh002400@naver.com>
2021-05-31 20:05:53 +0900

    home api added

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,3 +1,13 @@
 import express, { Request, Response } from "express";
-
+import list from "../models/list";
 const router = express.Router();
+
+router.get("/", async(res, req) => {
+  try{
+    const product = await list.find()
+    res.json(product);
+  } catch(error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+}) 
